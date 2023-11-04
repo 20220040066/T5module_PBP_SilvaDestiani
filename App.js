04a72f9b00aab581da55ebf51abd35a91947cd65
Silvaaa destiani <124055481+20220040066@users.noreply.github.com>
2023-11-04 21:01:23 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+const fullName= require('./module1');
+const myAge= require('./module2');
+const myHobby= require('./module3');
+const myWork= require('./module4');
+
+console.log(fullName('Silva destiani'));
+console.log(myAge('19 tahun'));
+console.log(myHobby('bernyanyi,membaca,'));
+console.log(myWork('guru'))
